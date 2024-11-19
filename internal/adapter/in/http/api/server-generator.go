643b090d786cc52dd17e8f06bfb4feb90c695849
Werner Dijkerman <werner@dj-wasabi.nl>
2024-11-19 20:39:14 +0100

    Rename of files to show that they belong together

--- internal/adapter/in/http/api/server-generator.go
+++ internal/adapter/in/http/api/server-generator.go
@@ -1,3 +1,3 @@
 package api
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config server.cfg.yaml ../../../../../api/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config server-generator.cfg.yaml ../../../../../api/api.yaml
